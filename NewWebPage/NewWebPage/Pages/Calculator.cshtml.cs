c7651ec3603c8425ecdc2d83e0556f1b1c0a1a63
SupaDupaGuy <70619636+SupaDupaGuy@users.noreply.github.com>
2021-01-12 23:54:56 -0500

    Sumarry

---
+++ NewWebPage/NewWebPage/Pages/Calculator.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewWebPage.Pages
+{
+    public class CalculatorModel : PageModel
+    {
+        int numQueries = 0;
+        public void OnPost()
+        {
+            string aname = "I worked!";
+            ViewData["name"] = aname;
+
+            string Test = "Test";
+            ViewData["mytest"] = Test;
+        }
+
+        public void OnGet()
+        {
+            numQueries++;
+            var num1 = Request.Query["num1"];
+            if (String.IsNullOrEmpty(num1) && numQueries > 1)
+            {
+                string aname = "You must enter in 2 numbers!";
+                ViewData["view1"] = aname;
+                numQueries = 5;
+            }
+            
+
+            numQueries++;
+            ViewData["numQ"] = numQueries;
+            
+        }
+    }
+}
